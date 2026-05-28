daa6531592b6c9d7e6868ea121730133d8fc8f62
kaito0311 <tanminh8b@gmail.com>
2026-05-28 20:26:20 +0700

    feat: add version 1

---
+++ test/widget_test.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:fqa/main.dart';
+
+Future<GameController> _controller() async {
+  final controller = GameController(MemoryProgressStore());
+  await controller.load();
+  return controller;
+}
+
+Future<void> _pumpApp(WidgetTester tester, GameController controller) async {
+  tester.view.physicalSize = const Size(426, 899);
+  tester.view.devicePixelRatio = 1;
+  addTearDown(tester.view.resetPhysicalSize);
+  addTearDown(tester.view.resetDevicePixelRatio);
+  await tester.pumpWidget(MainApp(controller: controller));
+  await tester.pump();
+}
+
+void main() {
+  testWidgets('home buttons route to story and collection', (tester) async {
+    final controller = await _controller();
+    await _pumpApp(tester, controller);
+
+    expect(find.text('FolkQuest'), findsOneWidget);
+
+    await tester.tap(find.text('Bắt đầu'));
+    await tester.pump();
+    expect(controller.view, AppView.story);
+    expect(find.text('Chia gia tài'), findsOneWidget);
+
+    controller.exitToHome();
+    await tester.pump();
+    await tester.tap(find.text('Bộ sưu tập'));
+    await tester.pump();
+    expect(controller.view, AppView.collection);
+    expect(find.text('Bộ sưu tập'), findsOneWidget);
+  });
+
+  testWidgets('discussion continue advances to options', (tester) async {
+    final controller = await _controller();
+    controller.startOrResume();
+    await _pumpApp(tester, controller);
+
+    await tester.tap(find.byKey(const ValueKey('icon_Tiếp tục')));
+    await tester.pump();
+
+    expect(controller.currentNode.type, StoryNodeType.options);
+    expect(find.text('Nhận cây khế'), findsOneWidget);
+  });
+
+  testWidgets('option choice changes karma and records selected choice', (
+    tester,
+  ) async {
+    final controller = await _controller();
+    controller.startOrResume();
+    controller.advance();
+    await _pumpApp(tester, controller);
+
+    await tester.tap(find.text('Nhận cây khế'));
+    await tester.pump();
+
+    expect(controller.karma, 1);
+    expect(controller.selectedChoices, contains('Nhận cây khế'));
+    expect(controller.unlockedCollectibleIds, contains('starfruit'));
+    expect(controller.currentNode.id, 'bag_choice');
+  });
+
+  testWidgets('unlock collectible adds item to persisted state', (
+    tester,
+  ) async {
+    final store = MemoryProgressStore();
+    final controller = GameController(store);
+    await controller.load();
+    controller.currentNodeId = 'feather_unlock';
+    controller.startOrResume();
+    await _pumpApp(tester, controller);
+
+    await tester.tap(find.text('Tiếp tục'));
+    await tester.pump();
+
+    expect(controller.unlockedCollectibleIds, contains('feather'));
+    expect(store.snapshot?.unlockedCollectibles, contains('feather'));
+  });
+
+  testWidgets('final restart clears current run state', (tester) async {
+    final controller = await _controller();
+    controller
+      ..currentNodeId = 'enough_ending'
+      ..completedEndingId = 'enough'
+      ..karma = 3
+      ..selectedChoices = ['Nhận cây khế'];
+    controller.startOrResume();
+    await _pumpApp(tester, controller);
+
+    await tester.tap(find.text('Chơi lại'));
+    await tester.pump();
+
+    expect(controller.currentNodeId, StoryRepository.startNodeId);
+    expect(controller.karma, 0);
+    expect(controller.selectedChoices, isEmpty);
+    expect(controller.view, AppView.story);
+  });
+
+  testWidgets('collection filters show all opened and locked items', (
+    tester,
+  ) async {
+    final controller = await _controller();
+    controller.unlockedCollectibleIds = {'bag3', 'starfruit'};
+    controller.openCollection();
+    await _pumpApp(tester, controller);
+
+    expect(find.byType(CollectibleCard), findsNWidgets(9));
+
+    await tester.tap(find.byKey(const ValueKey('filter_Đã mở')));
+    await tester.pump();
+    expect(controller.collectionFilter, CollectionFilter.opened);
+    expect(find.byType(CollectibleCard), findsNWidgets(2));
+
+    await tester.tap(find.byKey(const ValueKey('filter_Chưa mở')));
+    await tester.pump();
+    expect(controller.collectionFilter, CollectionFilter.locked);
+    expect(find.byType(CollectibleCard), findsNWidgets(7));
+  });
+}
