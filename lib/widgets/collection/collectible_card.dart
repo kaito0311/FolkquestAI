921277f84c883966f03383b9a9f3da0555b143ac
kaito0311 <tanminh8b@gmail.com>
2026-08-07 23:55:21 +0700

    fix: minors syntax and warning log

--- lib/widgets/collection/collectible_card.dart
+++ lib/widgets/collection/collectible_card.dart
@@ -40,13 +40,13 @@ class CollectibleCard extends StatelessWidget {
                         child: Opacity(
                           opacity: unlocked ? 1 : 0.28,
                           child: FqaAssetImage(
-                              collectible.assetName,
-                              fallback: Icon(
-                                unlocked ? Icons.auto_awesome : Icons.lock,
-                                color: const Color(0xff6a481d),
-                                size: 46,
-                              ),
+                            collectible.assetName,
+                            fallback: Icon(
+                              unlocked ? Icons.auto_awesome : Icons.lock,
+                              color: const Color(0xff6a481d),
+                              size: 46,
                             ),
+                          ),
                         ),
                       ),
                     ),
